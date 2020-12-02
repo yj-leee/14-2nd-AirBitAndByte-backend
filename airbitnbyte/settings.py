f0d7f5863ea0dbe4031b5ab043a30fb1f8ac22a4
Amuse <amusesla@gmail.com>
2020-12-02 17:37:49 +0900

    [fix] model 수정사항반영

--- airbitnbyte/settings.py
+++ airbitnbyte/settings.py
@@ -27,7 +27,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-	'corsheaders',
+    'corsheaders',
+    'user',
+    'map',
+    'property',
+    'reservation',
 ]
 
 MIDDLEWARE = [
--- user/models.py
+++ user/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+class TimeStampedModel(models.Model):
+     created_at = models.DateTimeField(auto_now_add=True)
+
+     class Meta:
+         abstract = True
+
+
+class User(TimeStampedModel):
+    email        = models.EmailField()
+    phone_number = models.CharField(max_length=200)
+    image_url    = models.URLField(max_length=1000, null=True)
+    first_name   = models.CharField(max_length=200)
+    last_name    = models.CharField(max_length=200)
+    password     = models.CharField(max_length=1000)
+
+    class Meta:
+        db_table = 'users'
+
+    def __str__(self):
+        return self.email
+
+
+class Bookmark(models.Model):
+    user     = models.ForeignKey('User', on_delete=models.CASCADE)
+    property = models.ForeignKey('property.Property', on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'bookmarks'
