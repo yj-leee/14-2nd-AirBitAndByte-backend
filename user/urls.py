d6e90935b73315d6bd47fd95fb2130a2282008af
yj-leee <yj.leee1010@gmail.com>
2021-02-03 14:27:08 +0900

    user views.py 수정

--- user/urls.py
+++ user/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path, include
 
-from .views import LoginView, SocialLoginView, RegisterView, BookmarkView
+from .views import LogInView, SocialLoginView, SignUpView, BookmarkView
 
 urlpatterns = [
     path('socialLogin', SocialLoginView.as_view()),
-    path('register', RegisterView.as_view()),
-    path('login', LoginView.as_view()),
+    path('signup', SignUpView.as_view()),
+    path('login', LogInView.as_view()),
     path('bookmark', BookmarkView.as_view()),
 
 ]
